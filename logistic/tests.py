099e21a68dbcf6112b89ba2b937197232b336ba7
Romanchiller <VacuuM2007@yandex.ru>
2023-10-29 23:19:56 +0500

    Commit

---
+++ logistic/tests.py
@@ -0,0 +1,10 @@
+from unittest import TestCase
+
+from rest_framework.test import APIClient
+
+class TestSmth(TestCase):
+    def sample_test(self):
+        client = APIClient()
+        url = '/api/v1/stocks/'
+        response = client.get(url)
+        self.assertEqual(response.status_code, 200)
