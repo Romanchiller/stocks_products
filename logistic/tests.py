679fef03b9fe3d95667974a0ae7496d38548cb35
Romanchiller <VacuuM2007@yandex.ru>
2023-10-30 03:11:32 +0500

    Fix test4

--- logistic/tests.py
+++ logistic/tests.py
@@ -1,12 +1,10 @@
-from unittest import TestCase
 import pytest
 from rest_framework.test import APIClient
 
 
 @pytest.mark.django_db
-class TestSmth(TestCase):
-    def sample_test(self):
-        client = APIClient()
-        url = '/api/v1/stocks/'
-        response = client.get(url)
-        self.assertEqual(response.status_code, 200)
+def sample_test(self):
+    client = APIClient()
+    url = '/api/v1/stocks/'
+    response = client.get(url)
+    self.assertEqual(response.status_code, 200)
